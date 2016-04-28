2fb8db5e001f049991960933af0565309e9110f1
MrBerts <berthier.gllm@gmail.com>
2016-04-28 11:45:31 +0200

    mongodb en local

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 var db;
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+mongodb.MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost/local', function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
