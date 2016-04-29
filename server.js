299d24d63b001e5635fa209db1d370da9bb74d0c
Adrien Menella <adrien.menella@gmail.com>
2016-04-29 11:42:04 +0200

    commentaire sur les headers

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ var app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-// enable CORS
+/*// enable CORS
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Content-Type", "application/json");
   next();
-});
+});*/
 
 // *** main routes *** //
 app.use(routes);
