118d3b83c91cf4a9f5a0714136d31cc7b64fb903
Adrien Menella <adrien.menella@gmail.com>
2016-04-29 11:30:57 +0200

    add header content-type

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Content-Type", "application/json");
   next();
 });
 
